af14aa06367f9089426b37c92e867573e9ca1e46
Shai Gewurtz <shaigtz@gmail.com>
2024-09-30 14:42:17 +0300

    Store match results in the database. MongoDB, either True or False

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from utils import parse_years_of_experience
+from bson import ObjectId
 
 # Connect to remote database
 try:
@@ -11,57 +12,96 @@ except Exception as e:
     print(f"Failed to connect to MongoDB: {e}")
 
 
-# Services that fetch data
+# This method fetches all jobs
 def fetchJobs():
     jobs = job_search_db.jobs.find().limit(1)
     return jobs
 
-def fetchCVs():
-    cvs = CV_db.CV_collection.find().limit(10)
-    return cvs
-
+# This method returns all CV's based on criteria
 def fetchCVs(criteria):
     cvs = CV_db.CV_collection.find(criteria)
     return cvs
 
-# Matches 
+# Creates new matches collection with a match record for each job and candidate (job_id_ candidate_id, False)
+def clean_data():
+        job_search_db.matches.drop()
+        matches = job_search_db.matches
+
+        jobs = fetchJobs()
+    
+        # Update table matches (job_id, candidate_id, match)
+        for job in jobs:
+            candidates = fetchCVs({})
+            job_id = ObjectId(job["_id"])
+            # print("Job_Id: " + str(job_id))
+
+            for candidate in candidates:
+                candidate_id = ObjectId(candidate['_id']) 
+                # print("Candidate_id: " + str(candidate_id))
+                document = {"job_id": job_id, "candidate_id": candidate_id, "match": False}
+                matches.insert_one(document)
+
+
+
+# Returns matching CV's to job based on years of experience 
 def match(job):
     min_years_experience = parse_years_of_experience(job["minimum_experience"])
-
-
-    print("Min Years Of Experience: " + str(min_years_experience))
-    
     criteria = {"years_of_experience": {"$gte": min_years_experience}}
     candidates = fetchCVs(criteria)
 
     return candidates
 
+# Updates matches collection given job_id, candidate_id and match value
+def score(job_id, candidate_id, match):
+    matches = job_search_db.matches
+    job_id = ObjectId(job_id)
+    candidate_id = ObjectId(candidate_id) 
 
+    filter_query = {
+        'job_id': job_id,  # First condition
+        'candidate_id': candidate_id   # Second condition
+    }
+
+    update_operation = {
+        '$set': {'match': match}  # Update or set new values
+    }
+
+    matches.update_one(filter_query, update_operation)
+
+# Main method
 def main():
+    # Update collection matches (job_id, candidate_id, False)
+    clean_data()
+
     jobs = fetchJobs()
     matches = {}
 
+    # For each job fetch candidates that meet criteria 
+    # (cv.years_of_experience >= job.minimal_experience_years
+    # cv.field_of_expertise in job.field_of_expertise
     for job in jobs:
         candidates = match(job)
         job_field_of_expertise = job["field_of_expertise"]
-        job_id = job["_id"]
+        job_id = str(job["_id"])
 
 
+        # For each candidate that meets minimum experience years
         for candidate in candidates:
-            id = candidate['_id']
-        
-            parse_years_of_experience = str(candidate["years_of_experience"])
+            candidate_id = str(candidate['_id'])
 
-            print("id: " + str(id))
-            print( "years of experience: " + parse_years_of_experience)
+            # Verifiy that at least one of cv.field_of_expertise matches at least one jobs.field_of_expertise
+            fields_of_expertise = candidate["field_of_expertise"]
+            # print(fields_of_expertise)
 
-            # Decide if field_of_expertise 
-            field_of_expertise = candidate["field_of_expertise"]
-
-            for field in field_of_expertise:
+            for field in fields_of_expertise:
                 # Check if field in job.field_of_expertise
                 if (field in job_field_of_expertise):
-                    matches[job_id].append(candidate)
+                    print (f"Found a match {job_id}, {candidate_id}")
+                    if job_id in matches:
+                        matches[job_id].append(candidate)
+                    else:
+                        matches[job_id] = [candidate]
+                    score(job_id, candidate_id, True)
                     break
 
 
